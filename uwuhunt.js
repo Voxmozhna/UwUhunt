43e9509caf19756f8f8d838e270e3ec7156dcf83
Voxmozhna <kagespiser@hotmail.com>
2021-09-12 18:47:06 +0200

    base: added skeleton to uwuhunt

---
+++ uwuhunt.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('scroll', function(){
+    document.querySelectorAll('[role="gridcell"]').forEach(function (el){
+        let header = el.getElementsByClassName("hBRZNM")[0];        
+        let span = header.getElementsByTagName('span')[0];
+        if(!span.hasAttribute('class', 'UwU')){
+            let pfpImage = el.getElementsByClassName("Asset--anchor")[0]; 
+            src = pfpImage.href;
+            let uwuId = src.substring(src.lastIndexOf('/') + 1)
+            span.innerHTML = "Rarity: #" + uwuId;
+            span.setAttribute("class", "UwU"); 
+            span.style.color = "#FF66B6";
+        }
+    });
+});
+
+
+
